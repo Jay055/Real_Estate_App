24ce7f009566a0dd905e2e92a045c7b197757b86
jay055 <nwachukwuuche@gmail.com>
2020-07-01 23:59:00 +0200

    Edited Room Filter

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -1,10 +1,54 @@
-import React from 'react'
+import React from 'react';
+import {useContext} from 'react';
+import {RoomContext} from '../Context';
+import Title from '../components/Title';
 
-export default function RoomFilter() {
+
+// Get Unique values (arrayofitems, types of unique values)
+const getUnique = (items,value) => {
+  // Set Only accepts unique values 
+  return [...new Set(.map(item => item[value]))];
+}
+
+// Get props from RoomContainer
+export default function RoomFilter(rooms) {
+  console.log(rooms);
+  // Using Hooks
+  const context = useContext(RoomContext); 
+  const {
+    handleChange, type, capacity, price, minPrice, maxPrice, minSize, maxSize, breakfast, pets } =  context;  
+
+    // get unique types 
+    // let types = getUnique(rooms, 'type');
+    // all types 
+    // types = ['all',...types];
+    // map to jsx
+    // types = types.map((item,index)=>{
+    //   return <option value={item} key={index} />
+    // })
+  
   return (
-    <div>
-      Room Filter
-    </div>
+    <section className="filter-container">
+      <Title tile="seach rooms"/>
+      <form className="filter-form">
+        {/* select type  */}
+        <div className="form-group">
+          <label htmlFor="type">room type</label>
+        
+        <select 
+        name="type"
+        id="type"
+        value='TYE'
+        // {type}
+        className="form-control"
+        onChange={handleChange}
+        >
+          {/* {types} */}
+        </select>
+        </div>
+        {/*  end select type  */}
+      </form>
+    </section>
   )
 }
 
